0b25a756b7ac958ae4b77f1f05fdc87522b1d31b
Ryu Sato <ryu@weseek.co.jp>
2018-03-31 21:31:04 +0900

    modified comment when start app

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ server.get('*', (req, res) => {
 
 server.listen(3000, (err) => {
   if (err) throw err
-  console.log('> Ready on http://localhost:3000 as backend')
+  console.log('> Ready on http://localhost:3000')
 })
 
 module.exports = server;
