fdb985d8c8f7a6bcc4c8c957c4708ad4e83a71bf
Ryu Sato <ryu@weseek.co.jp>
2018-04-04 03:21:20 +0900

    form編集時にfocusが外れる問題を解決

--- src/employee/_employee_form.js
+++ src/employee/_employee_form.js
@@ -7,19 +7,24 @@ var HelpBlock = require('react-bootstrap/lib/HelpBlock');
 var Radio = require('react-bootstrap/lib/Radio');
 var Button = require('react-bootstrap/lib/Button');
 
+class FieldGroup extends React.Component {
+  render() {
+    var id = this.props.id;
+    var label = this.props.label;
+    var help = this.props.help;
+    return(
+      <FormGroup controlId={id}>
+        <ControlLabel>{label}</ControlLabel>
+        <FormControl {...this.props} />
+        {help && <HelpBlock>{help}</HelpBlock>}
+      </FormGroup>
+    );
+  }
+}
+
 class EmployeeForm extends React.Component {
 
   render() {
-    function FieldGroup({ id, label, help, ...props }) {
-      return (
-        <FormGroup controlId={id}>
-          <ControlLabel>{label}</ControlLabel>
-          <FormControl {...props} />
-          {help && <HelpBlock>{help}</HelpBlock>}
-        </FormGroup>
-      );
-    }
-
     const employee = this.props.employee || {};
     const id = (employee.id ? <div>ID: {employee.id}</div> : '');
     const method = this.props.method || "POST";
@@ -28,18 +33,18 @@ class EmployeeForm extends React.Component {
     return(
       <form action={action} method='post'>
         {id}
-        <FieldGroup id="name" type="text" label="Name" name='name' value={employee.name} placeholder="Input Employee's Name" onChange={cb} />
-        <FieldGroup id="Department" type="text" label="Department" name='department' value={employee.department} placeholder="" onChange={cb} />
+        <FieldGroup id="name" label="Name" help="" type="text" name='name' value={employee.name} placeholder="Input Employee's Name" onChange={cb} />
+        <FieldGroup id="Department" label="Department" help="" type="text" name='department' value={employee.department} placeholder="" onChange={cb} />
         <FormGroup controlId="Gender">
           <ControlLabel>Gender</ControlLabel>
           <Radio name="gender" inline defaultValue='male' checked={employee.gender==="male"} onChange={cb}>male</Radio>
           <Radio name="gender" inline defaultValue='female' checked={employee.gender==="female"}  onChange={cb}>female</Radio>
           <Radio name="gender" inline defaultValue='other' checked={employee.gender!=="male"&&employee.gender!=="female"} onChange={cb}>other</Radio>
         </FormGroup>
-        <FieldGroup id="Birthday" type="text" label="Birthday" name='birth' value={employee.birth} placeholder="Input Employee's Birthday" onChange={cb} />
-        <FieldGroup id="Joined Date" type="text" label="Joined Date" name='joined_date' value={employee.joined_date} placeholder="Input Employee's Joined Date" onChange={cb} />
-        <FieldGroup id="Payment" type="text" label="Payment" name='payment' value={employee.payment} placeholder="Input Employee's Payment" onChange={cb} />
-        <FieldGroup id="Note" type="text" label="Note" name='note' value={employee.note} placeholder="Input Note" onChange={cb} />
+        <FieldGroup id="Birthday" label="Birthday" help="" type="text" name='birth' value={employee.birth} placeholder="Input Employee's Birthday" onChange={cb} />
+        <FieldGroup id="Joined Date" label="Joined Date" help="" type="text" name='joined_date' value={employee.joined_date} placeholder="Input Employee's Joined Date" onChange={cb} />
+        <FieldGroup id="Payment" label="Payment" help="" type="text" name='payment' value={employee.payment} placeholder="Input Employee's Payment" onChange={cb} />
+        <FieldGroup id="Note" label="Note" help="" type="text" name='note' value={employee.note} placeholder="Input Note" onChange={cb} />
         <Button type="submit" value="Submit">Submit</Button>
       </form>
     );
